0c5fed49ee83f3687fbfe5cf372a9c41a1ab9734
jlkiri <interalia.dev@gmail.com>
2020-04-15 17:31:29 +0900

    Remove web_modules from source code

--- looc-loader/src/web_modules/react-dom.js
+++
@@ -1,2 +0,0 @@
-import './common/react-d0ba784a.js';
-export { _ as __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED, c as createPortal, R as default, f as findDOMNode, a as flushSync, h as hydrate, r as render, u as unmountComponentAtNode, b as unstable_batchedUpdates, d as unstable_createPortal, e as unstable_renderSubtreeIntoContainer, v as version } from './common/react-dom-529cd6c9.js';
